e1896f61e0c160f964e629b2479eb2b7104ee769
Sanskar Singh <sanskar.singh@testrigor.com>
2024-03-09 05:19:38 +0530

    final tinckering

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ limit : "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts',postRoutes);
-const CONNECTION_URL = "mongodb+srv://Ken:ken123@cluster0.0ygaclo.mongodb.net/";
+const CONNECTION_URL = process.env.URI;
 
 const PORT = process.env.PORT || 5000;
 
